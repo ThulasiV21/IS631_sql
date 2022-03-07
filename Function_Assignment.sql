17905e6df02279e433df7852c6768a8df8f4ecc0
thulasigovardhan <thulasigovardhan@gmail.com>
2022-03-07 18:48:52 -0500

    Function assignment final

--- Function_Assignment.sql
+++ Function_Assignment.sql
@@ -18,7 +18,7 @@ SELECT ba.teamID, ba.playerID, dbo.FullName(ba.playerID) AS [FUll Name],
 SUM(ba.H) AS [Total Hits], 
 SUM(ba.AB) AS [Total At Bats],
 CONVERT(decimal(5,4), SUM(ba.H) * 1.0 / SUM(ba.AB)) AS [Batting Average],
-Team_BA_rank, BA_rank 
+Team_BA_rank AS [Team Batting Rank], BA_rank as [All Batting Rank] 
 from (select teamid, playerid, cast(sum(h*1.0)/sum(ab) as decimal(5,4)) as 
 player_ba,  
 rank() over (partition by teamid order by 
@@ -46,20 +46,3 @@ order by Team_BA_rank, BA_rank;
 -- WHERE 
 -- HAVING SUM(b.H) > 150
 -- ORDER BY [Team Batting rank], [All Batting rank];
-
-
-(select teamid, playerid, cast(sum(h*1.0)/sum(ab) as decimal(5,4)) as 
-player_ba,  
-rank() over (partition by teamid order by 
-(sum(h*1.0)/sum(ab)) desc) as ba_rank 
-  from batting 
-  group by teamid, playerid 
-  having sum(ab) > 0) A, 
-  (select teamid, sum(h) as h, sum(ab) as ab, sum(h*1.0)/sum(ab) as 
-team_BA,  
-rank() over (order by sum(h*1.0)/sum(ab) desc) as 
-Team_ba_rank 
-    from teams 
-group by teamid) B 
-where a.teamid = b.teamid 
-order by Team_BA_rank, BA_rank
